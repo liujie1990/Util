6284180d569e2e60987f1ccd5eb2fb5accdd8eb9
刘杰 <709394597@qq.com>
2016-07-01 18:34:20 +0800

    event.js

---
+++ event.js
@@ -0,0 +1,54 @@
+var EventUtil = {
+    //添加事件处理程序
+    addHandler: function(element, type, handler){
+        if (element.addEventListener){
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent){
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+    //获取按键的值
+    getCharCode: function(event){
+        if (typeof event.charCode == "number"){
+            return event.charCode;
+        } else {
+            return event.keyCode;
+        }
+    },
+    //获取事件对象
+    getEvent: function(event){
+        return event ? event : window.event;
+    },
+    //获取目标对象
+    getTarget: function(event){
+        return event.target || event.srcElement;
+    },
+    //阻止默认事件
+    preventDefault: function(event){
+        if (event.preventDefault){
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+    //删除事件处理程序
+    removeHandler: function(element, type, handler){
+        if (element.removeEventListener){
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent){
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
+        }
+    },
+    //阻止事件冒泡
+    stopPropagation: function(event){
+        if (event.stopPropagation){
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    }
+};
